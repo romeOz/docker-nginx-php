8832fb0bf2fcc2d9434d183492f64a37ec903009
romeOz <serggalka@gmail.com>
2015-10-07 22:28:51 +0300

    Minor adjustments.

--- test.sh
+++ test.sh
@@ -5,66 +5,66 @@ set -e
 echo "-- Building Nginx + PHP 5.6 image"
 docker build -t php-5.6 5.6/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 php-5.6; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -c 'Hello world'
 docker exec -it app php -v | grep -c 'PHP 5.6'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f php-5.6
 
-echo ""
-echo ""
+echo
+echo
 echo "-- Building Nginx + PHP 5.5 image"
 docker build -t php-5.5 5.5/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 php-5.5; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -c 'Hello world'
 docker exec -it app php -v | grep -c 'PHP 5.5'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f php-5.5
 
 
-echo ""
-echo ""
+echo
+echo
 echo "-- Building Nginx + PHP 5.4 image"
 docker build -t php-5.4 5.4/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 php-5.4; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -c 'Hello world'
 docker exec -it app php -v | grep -c 'PHP 5.4'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f php-5.4
 
 
-echo ""
-echo ""
+echo
+echo
 echo "-- Building Nginx + PHP 5.3 image"
 docker build -t php-5.3 5.3/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 php-5.3; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -c 'Hello world'
 docker exec -it app php -v | grep -c 'PHP 5.3'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f php-5.3
 
-echo ""
+echo
 echo "-- Done"
